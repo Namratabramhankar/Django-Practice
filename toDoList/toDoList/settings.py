06c36aab5ed49d0c973b1b99e28ef2ead7c82015
Namrata Bramhankar <namratabramhankar@gmail.com>
2020-03-27 19:10:38 -0700

    ToDoApp

--- toDoList/toDoList/settings.py
+++ toDoList/toDoList/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos.apps.TodosConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'toDoList.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER':'root',
+        'PASSWORD':'1999@Namrata',
+        'HOST':'',
+        'PORT':''
     }
 }
 
--- toDoList/toDoList/urls.py
+++ toDoList/toDoList/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.conf.urls import include,url
 
 urlpatterns = [
-    url(r'^$', include('todos.urls')),
+   # url(r'^$', include('todos.urls')),
     url(r'^todos/', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
